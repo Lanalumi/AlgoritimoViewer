226d4f1feb412fd78d473e1b349e12dd88bbc337
Lanalumi <lanalumi97@gmail.com>
2025-04-15 01:11:46 +0000

    4 Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.scss";
+import Header from "./components/Header";
 
 function App() {
  
   return (
-    <div className = "app">01</div>
+    <div className="app"><Header/></div>
   )
 }
 export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.scss";
+
+const Header = () => (
+    <header className="header">
+
+        <div className="fake-menu">
+            <div className="fake-menu__dot fake-menu__dot--emph"></div>
+            <div className="fake-menu__dot"></div>
+            <div className="fake-menu__dot"></div>
+
+        </div>
+
+        <h1 className="title">Algorithm Visualizer</h1>
+    </header>
+)
+
+export default Header;
